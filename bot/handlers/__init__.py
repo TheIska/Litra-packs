820bc684bf9b718964e113e320292a23972edfc4
TheIska <Khasanshiniskander@gmail.ru>
2026-06-21 03:02:58 +0300

    Добавлен helpers.py с shuffle_question, обновлён duel.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,22 +1,22 @@
+# bot/handlers/__init__.py
 from .start import start, help_command, show_coins, shop, report_error, forward_to_admin
 from .pack import free_pack, small_pack, medium_pack, large_pack
-from .collection import show_collection
+from .album import show_album, album_navigation, find_card_by_number, handle_card_number_input
 from .duel import (
-    duel_command,
-    answer_callback,
-    stop_duel_command,
-    handle_hero_selection,
+    duel_command, 
+    answer_callback, 
+    stop_duel_command, 
+    handle_hero_selection, 
     handle_invite_link,
-    duel_random,
-    duel_friend,
-    duel_friend_select,
-    duel_bot,
-    duel_accept,
-    duel_decline,
-    duel_invite,
-    copy_link,
-    share_link,
-    cancel_duel
+    duel_random, 
+    duel_friend, 
+    duel_friend_select, 
+    duel_bot, 
+    duel_accept, 
+    duel_decline, 
+    duel_invite, 
+    copy_link, 
+    share_link
 )
 from .admin import add_coins_command
 from .quiz import quiz_command, quiz_answer_callback, stop_quiz_command
